1f504451e86cb68116c56c1ede430ac78172fbda
AnTi3z <anti3z@anti3z.ru>
2017-10-20 14:38:27 +0300

    Add \U2060 UTF-8 code to strip_emoji() function (utils.py)

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import re
 
 emoji_pattern = re.compile("[\U0001F600-\U0001F64F\U0001F300-\U0001F5FF"
-                               "\U0001F680-\U0001F6FF\U0001F1E0-\U0001F1FF]+")
+                           "\U0001F680-\U0001F6FF\U0001F1E0-\U0001F1FF\U00002060]+")
 
 
 def str_human_int(x):
