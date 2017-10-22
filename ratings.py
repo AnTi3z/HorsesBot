2b7d5cbba9e988ad742c778ff41bc4d2de9a41d0
AnTi3z <anti3z@anti3z.ru>
2017-10-22 23:48:20 +0300

    Add ratings module with funcions generating ratings texts

---
+++ ratings.py
@@ -0,0 +1,78 @@
+from operator import itemgetter
+
+import db_wrap
+from utils import *
+
+
+def players_gold(user_id):
+    stat = db_wrap.get_players_stat()
+    sorted_stat = sorted(stat, key=itemgetter('money'), reverse=True)[:10]
+    result = ['💰Золото\n\n']
+    for row in sorted_stat:
+        result.append('`{:<14.14}  {:>5}💰`\n'.format(strip_emoji(row['first_name']), str_human_int(row['money'])))
+    return ''.join(result)
+
+
+def players_level(user_id):
+    stat = db_wrap.get_players_stat()
+    sorted_stat = sorted(stat, key=itemgetter('level', 'money'), reverse=True)[:10]
+    result = ['⚜️Уровень\n\n']
+    for row in sorted_stat:
+        result.append('`{:<16.16}  {:>3}⚜`\n'.format(strip_emoji(row['first_name']), row['level']))
+    return ''.join(result)
+
+
+def players_wins(user_id, sort_rate=False):
+    if sort_rate:
+        def sort_key(x):
+            if x['bets_cnt'] >= 100:
+                return x['wins']/x['bets_cnt'], 0
+            else:
+                return 0, x['wins']/x['bets_cnt']
+    else:
+        sort_key = itemgetter('wins')
+    stat = db_wrap.get_players_stat()
+    sorted_stat = sorted(stat, key=sort_key, reverse=True)[:10]
+    result = ['🏆Победы\n\n']
+    for row in sorted_stat:
+            percent = round(row['wins']*100/row['bets_cnt'], 1)
+            result.append('`{:<12.12} {:>3}/{:<3}({}%)`\n'.format(strip_emoji(row['first_name']),
+                                                                  row['wins'], row['bets_cnt'], percent))
+    return ''.join(result)
+
+
+def players_prizes(user_id, sort_rate=False):
+    if sort_rate:
+        def sort_key(x):
+            if x['bets_cnt'] >= 100:
+                return x['prizes']/x['bets_cnt'], 0
+            else:
+                return 0, x['prizes']/x['bets_cnt']
+    else:
+        sort_key = itemgetter('prizes')
+    stat = db_wrap.get_players_stat()
+    sorted_stat = sorted(stat, key=sort_key, reverse=True)[:10]
+    result = ['🥇🥈🥉Медали\n\n']
+    for row in sorted_stat:
+            percent = round(row['prizes'] * 100 / row['bets_cnt'], 1)
+            result.append('`{:<12.12} {:>3}/{:<3}({}%)`\n'.format(strip_emoji(row['first_name']),
+                                                                  row['prizes'], row['bets_cnt'], percent))
+    return ''.join(result)
+
+
+def players_bets(user_id, sort_sum=False):
+    if sort_sum:
+        sort_key = itemgetter('bets_sum')
+    else:
+        sort_key = itemgetter('bets_cnt')
+    stat = db_wrap.get_players_stat()
+    sorted_stat = sorted(stat, key=sort_key, reverse=True)[:10]
+    result = ['🥇🥈🥉Медали\n\n']
+    for row in sorted_stat:
+        if sort_sum:
+            result.append('`{:<15.15}  {:>4}💰`\n'.format(strip_emoji(row['first_name']),
+                                                          row['bets_sum']))
+        else:
+            result.append('`{:<16.16}  {:>4}`\n'.format(strip_emoji(row['first_name']),
+                                                        str_human_int(row['bets_cnt'])))
+    return ''.join(result)
